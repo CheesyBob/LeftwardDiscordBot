fd79e9729a21d508870fe75bcc6871f0bf215741
CheesyBob <76981690+CheesyBob@users.noreply.github.com>
2025-10-22 00:34:33 -0700

    dm update

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ const commands = [
   new SlashCommandBuilder()
     .setName('leftward')
     .setDescription('RESPONCE')
+    .setDMPermission(true)
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
